ef384028325eb3f08fa54e92583cdfa24d39ce45
khasanlee <xasaniskandarov2000@gmail.com>
2025-04-02 00:33:16 +0900

    setting up simulator and working with components and styling

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text style={styles.dummyText}>Another piece of text</Text>
+      </View>
+      <Text style={styles.dummyText}>Heldfo</Text>
+      <Text>its meee</Text>
+      <Button title='Tap ME' />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  dummyText: {
+    margin: 10,
+    fontWeight: 500,
+    borderWidth: 1,
+    borderColor:"red",
+    padding:10
+  }
 });
