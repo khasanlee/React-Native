cff0d30f60c13d94d7d49e16c7f23729b5cd2ce1
khasanlee <xasaniskandarov2000@gmail.com>
2025-04-02 19:56:26 +0900

    managing a list of course Goals. outputing entered goals

--- App.js
+++ App.js
@@ -2,13 +2,14 @@ import { useState } from "react";
 import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
-  const [enteredGoalText, setEnteredGoalText] = useState('')
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
   function goalInputHandler(enteredText) {
-    setEnteredGoalText(enteredText)
+    setEnteredGoalText(enteredText);
   }
 
   function addGoalHandler() {
-    console.log(enteredGoalText)
+    setCourseGoals((currentGoals) => [...currentGoals, enteredGoalText]);
   }
 
   return (
@@ -22,7 +23,7 @@ export default function App() {
         <Button title="Add Goal" onPress={addGoalHandler} />
       </View>
       <View style={styles.goalContainer}>
-        <Text>List of goals...</Text>
+        {courseGoals.map((goal)=><Text key={goal}>{goal}</Text>)}
       </View>
     </View>
   );
