7387ce4a9d937e35385c2b30ada61c8a2c450c50
khasanlee <xasaniskandarov2000@gmail.com>
2025-04-02 16:07:12 +0900

    deep dive to flexbox

--- App.js
+++ App.js
@@ -3,13 +3,41 @@ import { Button, StyleSheet, Text, View, TextInput } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.appContainer}>
-      <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder="Your course goal!" />
-        <Button title="Add Goal" />
+    <View 
+      style={{
+        padding:50,
+        flexDirection: 'row', 
+        width:'80%', 
+        height:300, 
+        justifyContent: 'space-around', 
+        alignItems:'stretch'
+      }}
+    >
+      <View 
+        style={{
+          backgroundColor: 'red',
+          flex: 4,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        <Text>1</Text>
       </View>
-      <View>
-        <Text>List of goals...</Text>
+      <View 
+        style={{
+          backgroundColor: 'blue',
+          flex: 3,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        <Text>2</Text>
+      </View><View 
+        style={{
+          backgroundColor: 'green',
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        <Text>3</Text>
       </View>
     </View>
   );
