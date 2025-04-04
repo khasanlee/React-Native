4e2557fd08956a145a62a84342ee16f512bf7caf
khasanlee <xasaniskandarov2000@gmail.com>
2025-04-04 15:54:45 +0900

    adding ripple effect to the button

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -2,11 +2,14 @@ import { StyleSheet, Text, View, Pressable } from "react-native";
 
 function GoalItem(props) {
   return (
-    <Pressable onPress={props.onDeleteItem.bind(this, props.id)}>
-      <View style={styles.goalList}>
+    <View style={styles.goalList}>
+      <Pressable
+        onPress={props.onDeleteItem.bind(this, props.id)}
+        style={({ pressed }) => pressed && styles.pressedItem}
+      >
         <Text style={styles.goalText}>{props.text}</Text>
-      </View>
-    </Pressable>
+      </Pressable>
+    </View>
   );
 }
 
@@ -14,13 +17,18 @@ export default GoalItem;
 
 const styles = StyleSheet.create({
   goalList: {
+      margin: 8,
+      backgroundColor: "red",
+      borderRadius: 10,
+    },
+    pressedItem: {
+        opacity: 0.5,
+        
+    },
+    goalText: {
     padding: 10,
-    margin: 8,
-    backgroundColor: "red",
-    borderRadius: 10,
-  },
-  goalText: {
     color: "white",
     textAlign: "center",
+
   },
 });
